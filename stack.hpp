c48c76b29994655bf91504f972132d0727e1f18e
iwillens <iwillens@student.42sp.org.br>
2021-10-15 20:48:22 -0300

    started stack

--- stack.hpp
+++ stack.hpp
@@ -6,19 +6,26 @@
 /*   By: iwillens <iwillens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 16:15:06 by iwillens          #+#    #+#             */
-/*   Updated: 2021/10/15 20:36:31 by iwillens         ###   ########.fr       */
+/*   Updated: 2021/10/15 20:48:15 by iwillens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
 # define STACK_HPP
 
+# include "vector.hpp"
+
 namespace ft
 {
-  template <typename Key, typename T, typename Compare = ft::less<Key>, typename Alloc = std::allocator<ft::pair<const Key, T> > >
-	class map
+    template <class T, class Container = ft::vector<T> >
+    class stack;
 	{
+        typedef T           value_type;
+        typedef Container   container_type;
+        typedef size_t      size_type;
 
+        explicit stack (const container_type& ctnr = container_type());
+bool empty() const;
 
     }
 }
